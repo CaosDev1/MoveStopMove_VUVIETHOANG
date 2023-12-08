a8b2196ac0ff08d41f1c6e6b62d6c5eed320566e
CaosDev1 <vhoang0327@gmail.com>
2023-12-08 21:43:28 +0700

    Dang lam Shop Skin UI

---
+++ Assets/_Game/Scripts/CanvasSkinShop.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasSkinShop : MonoBehaviour
+{
+    [SerializeField] private Button hatShopButton;
+    [SerializeField] private Button pantsShopButton;
+    [SerializeField] private Button sheildShopButton;
+    [SerializeField] private Button fullSetShopButton;
+}
